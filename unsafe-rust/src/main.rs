f8f882fad28e410d77b5060bbfb1d0dc18c643d4
h0pped <notawril@gmail.com>
2024-04-07 08:27:47 +0200

    unsafe-rust

---
+++ unsafe-rust/src/main.rs
@@ -0,0 +1,80 @@
+use core::slice;
+
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+unsafe trait Foo {}
+unsafe impl Foo for i32 {}
+fn main() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+
+    let r2 = &mut num as *mut i32;
+
+    // let address = 0x012345usize;
+
+    // let r3 = address as *const i32;
+
+    unsafe {
+        println!("r1 is {}", *r1);
+        println!("r2 is {}", *r2)
+    };
+
+    unsafe fn dangerous() {}
+
+    unsafe {
+        dangerous();
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    let (c, d) = split_at_mut(&mut r[..], 3);
+
+    assert_eq!(c, &mut [1, 2, 3]);
+    assert_eq!(d, &mut [4, 5, 6]);
+
+    unsafe {
+        println!("Absolute value of -3 according to C lang is {}", abs(-3));
+    }
+
+    add_to_count(3);
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}

4ea783c905b453377dbb9e473f934b187d9436a5
h0pped <notawril@gmail.com>
2024-04-13 15:20:54 +0200

    macros

---
+++ declarative_macros/src/lib.rs
@@ -0,0 +1,18 @@
+use proc_macro;
+
+#[some_attribute]
+pub fn some_name(input: TokenStream) -> TokenStream {}
+
+#[macro_export]
+macro_rules! vec {
+    ( $( $x:expr),*) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
